297eba02bc893e739369ba98421eda8314984e6e
Kingshuk Kundu <44145229+kingsamsrs@users.noreply.github.com>
2019-08-19 16:28:33 -0400

    Update guessGame.java

--- src/guessGame.java
+++ src/guessGame.java
@@ -1,5 +1,29 @@
+// ============================================================================
+//   Copyright 2019 Kingshuk Kundu
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// ============================================================================
+
 import java.util.Scanner;
 import java.util.Random;
+
+/**
+ * Encapsulates a simple guessing game. The computer generates
+ * a random number between 1 to 100 and the player has to guess  
+ * the number in least amount of tries
+ * @author Kingshuk Kundu
+ */
+
 public class guessGame {
 	public static void main(String[] args) {
 		Random rand= new Random();
